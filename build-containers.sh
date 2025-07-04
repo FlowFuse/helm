ad50bf4d011adb89cbbbe6ed0311c74fc7fd43da
Ben Hardill <ben@flowforge.com>
2025-07-04 11:57:42 +0100

    chore: rename flowforge-container
    linked to FlowFuse/admin#405

--- build-containers.sh
+++ build-containers.sh
@@ -6,7 +6,7 @@ REGISTRY=$1
 
 if [ ! -z "$REGISTRY" ]; then
 
-  docker build flowforge-container -t $REGISTRY/flowforge/forge-k8s:$TAG
+  docker build flowfuse-container -t $REGISTRY/flowforge/forge-k8s:$TAG
   docker build node-red-container -t $REGISTRY/flowforge/node-red:$TAG
   docker build file-server -t %REGISTRY/flowforge/file-server:$TAG
 
